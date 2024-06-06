8e62c7cfd273396ab36c49c368e56dff1965255b
gabyferr <ferreiragaby613@gmail.com>
2024-06-06 10:59:36 -0300

    feat: Creating the Add Order Function

--- shop/lib/pages/cart_page.dart
+++ shop/lib/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/cart_item.dart';
 import 'package:shop/model/cart.dart';
+import 'package:shop/model/order_list.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -56,7 +57,10 @@ class CartPage extends StatelessWidget {
                   ),
                   const Spacer(),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<OrderList>(context, listen: false).addOrder(cart);
+                      cart.clear();
+                    },
                     child: const Text(
                       'COMPRAR',
                       style: TextStyle(color: Colors.blue),
