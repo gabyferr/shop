04c7b7602801145a9085c8a17c499340ec558dbf
gabyferr <ferreiragaby613@gmail.com>
2024-06-07 07:48:10 -0300

    feat: Drawer creation

---
+++ shop/lib/components/app_drawer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text(
+              'Bem vindo Usuários!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.shop),
+            title: const Text('Loja'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(AppRoutes.home);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.payment),
+            title: const Text('Pedidos'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(AppRoutes.orders);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
